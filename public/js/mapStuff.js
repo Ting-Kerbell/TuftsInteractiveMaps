4a1fdbb04a28e6b669f58ff9f1a535664da4e298
jessicaxwang <jessicawang97@yahoo.com>
2018-10-13 08:33:21 -0400

    infowindow bug

--- public/js/mapStuff.js
+++ public/js/mapStuff.js
@@ -1,54 +1,87 @@
-      var map;
-      function initMap() {
-         var iconBase = 'img/';
-        var icons = {
-          bathroom: {
-            size: new google.maps.Size(20, 32),
-            url: iconBase + 'toilet.png'
+var map;
+function initMap() {
+  var markers = [];
+  var iconBase = 'img/';
+  var icons = {
+    bathroom: {
+      size: new google.maps.Size(20, 32),
+      url: iconBase + 'toilet.png'
 
-          },
-          food: {
-            size: new google.maps.Size(20, 32),
-            url: iconBase + 'food.png'
+    },
+    food: {
+      size: new google.maps.Size(20, 32),
+      url: iconBase + 'food.png'
 
-          },
-          academic: {
-            size: new google.maps.Size(20, 32),
-            url: iconBase + 'study.png'
+    },
+    academic: {
+      size: new google.maps.Size(20, 32),
+      url: iconBase + 'study.png'
 
-          },
-          parking: {
-            size: new google.maps.Size(20, 32),
-            url: iconBase + 'car.png'
+    },
+    parking: {
+      size: new google.maps.Size(20, 32),
+      url: iconBase + 'car.png'
 
-          }
-        };
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 15,
-          center: {lat: 42.4075, lng: -71.1190},
-          mapTypeId: 'terrain'
-        });
+    },
+    dog: {
+      size: new google.maps.Size(20, 32),
+      url: iconBase + 'paw.png'
+    }
+  };
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: {lat: 42.4075, lng: -71.1190},
+    mapTypeId: 'terrain'
+  });
 
-        var param = window.location.href;
-        param = param.substring(param.indexOf('service=') + 8, param.indexOf('&'));
-        var url =  "http://lit-everglades-77388.herokuapp.com/database.json?service="+ param;
-        console.log("param:", param);
-        var request = new XMLHttpRequest();
-        request.open("GET", url, true);
-         request.onreadystatechange = function() {
-           if (request.readyState == 4 && request.status == 200) {
-             response = request.responseText;
-             parsed = JSON.parse(response);
-             for (var i = 0; i < parsed.length; i++) {
-               var latLng = new google.maps.LatLng(parsed[i][1], parsed[i][2])
-               var marker = new google.maps.Marker({
-                 position: latLng,
-                 map: map,
-                 icon: icons[param].url
-               });
-             }
-           }
-         }
-         request.send();
+  var i, param = window.location.href;
+  param = param.substring(param.indexOf('service=') + 8, param.indexOf('&'));
+  var url =  "http://lit-everglades-77388.herokuapp.com/database.json?service="+ param;
+  console.log("param:", param);
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+   request.onreadystatechange = function() {
+     if (request.readyState == 4 && request.status == 200) {
+       response = request.responseText;
+       parsed = JSON.parse(response);
+       for (i = 0; i < parsed.length; i++) {
+         var latLng = new google.maps.LatLng(parsed[i][1], parsed[i][2])
+         var marker = new google.maps.Marker({
+           position: latLng,
+           map: map,
+           icon: icons[param].url,
+         });
+
+         console.log("before", marker.position);
+
+         markers.push(marker);
+         marker.setMap(map);
+       }
+
+       for (i = 0; i < parsed.length; i++) {
+         var marker = markers[i];
+         console.log(marker);
+         var title = parsed[i][0];
+         console.log(title);
+         var notes = parsed[i][4];
+         var contentString = "<h4>" + title + "</h4></br>" + "Gender neutral bathroom</br>" + notes;
+
+         var infoWindow = new google.maps.InfoWindow({
+           content: contentString
+         });
+         marker.addListener('click', function() {
+           marker
+           infoWindow.open(map, marker);
+         })
+       }
+     } else if (request.readyState == 4 && request.status != 200) {
+          console.log("Sorry - we couldn't fetch your information.");
+     }
+
+
+
+
+   }
+   request.send();
 
 }
