6be3785cd0d545b6621f9ea8037515169c8c82b4
jessicaxwang <jessicawang97@yahoo.com>
2018-10-13 06:36:33 -0400

    logging

--- public/js/mapStuff.js
+++ public/js/mapStuff.js
@@ -32,6 +32,7 @@
         var param = window.location.href;
         param = param.substring(param.indexOf('service=') + 8, param.indexOf('&'));
         var url =  "http://lit-everglades-77388.herokuapp.com/database.json?service="+ param;
+        console.log("param:", param);
         var request = new XMLHttpRequest();
         request.open("GET", url, true);
          request.onreadystatechange = function() {
