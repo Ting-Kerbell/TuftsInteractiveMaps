7bd59e58944d8666eeed48642cc9a2dba6494fc4
jessicaxwang <jessicawang97@yahoo.com>
2018-10-13 00:32:57 -0400

    reinit

--- server.js
+++ server.js
@@ -7,4 +7,11 @@ const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.get('/database.json', function(req, res) {
+    var service = req.query.service;
+    var user = req.query.user;
+
+    console.log(service, user);
+});
+
 server.listen(8080, () => console.log('App is running on port 8080'));
