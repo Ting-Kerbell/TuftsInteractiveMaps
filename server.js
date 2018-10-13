e0241b8626eabcc1a34490f827146e6523704c35
Steven <steven.song98@gmail.com>
2018-10-13 00:19:29 -0400

    all redirect logic done

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.get('/', function(req, res) {
+  res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+});
+
+app.get('/user', function(req, res) {
+  res.sendFile('user.html', {root: path.join(__dirname, 'public')});
+});
+
+app.get('/service', function(req, res) {
+  res.sendFile('service.html', {root: path.join(__dirname, 'public')});
+})
+
+app.get('/map', function(req, res) {
+  res.sendFile('mapTest.html', {root: path.join(__dirname, 'public')});
+})
+
+app.get('/:folder/:file', function(req, res) {
+  res.sendFile(req.params.file, {root: path.join(__dirname, 'public', req.params.folder)});
+});
 
 server.listen(8080, () => console.log('App is running on port 8080'));
