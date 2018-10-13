7330ba6d639d17d470a50f23fca849f7b59deab1
jessicaxwang <jessicawang97@yahoo.com>
2018-10-13 06:29:33 -0400

    backend logic for marker setting complete

--- public/js/mapStuff.js
+++ public/js/mapStuff.js
@@ -1,28 +1,26 @@
       var map;
-
-   
       function initMap() {
          var iconBase = 'img/';
         var icons = {
-          toilet: {
+          bathroom: {
             size: new google.maps.Size(20, 32),
             url: iconBase + 'toilet.png'
-            
+
           },
           food: {
             size: new google.maps.Size(20, 32),
             url: iconBase + 'food.png'
-            
+
           },
-          study: {
+          academic: {
             size: new google.maps.Size(20, 32),
             url: iconBase + 'study.png'
-            
+
           },
           parking: {
             size: new google.maps.Size(20, 32),
             url: iconBase + 'car.png'
-         
+
           }
         };
         map = new google.maps.Map(document.getElementById('map'), {
@@ -30,25 +28,29 @@
           center: {lat: 42.4075, lng: -71.1190},
           mapTypeId: 'terrain'
         });
-         var latLng = new google.maps.LatLng(42.4068,-71.1187)
 
-          var marker = new google.maps.Marker({
-            position: latLng,
-            map: map,
-            
-            icon: icons["toilet"].url
-          });
-        $.get("http://lit-everglades-77388.herokuapp.com/database.json?service="+type, function(data) {
-          results = data.parse();
-        for (var i = 0; i < results.length; i++) {
-          var coords = results[i].coordinates;
-          //var latLng = new google.maps.LatLng(coords[1],coords[0]);
-          var latLng = new google.maps.LatLng(results[i][1],results[i][2])
-          var marker = new google.maps.Marker({
-            position: latLng,
-            map: map,
-            icon: icons[type].icon
-          });
-        }
-      }); 
+        var param = window.location.href;
+        param = param.substring(param.indexOf('service=') + 8, param.indexOf('&'));
+        var url =  "http://lit-everglades-77388.herokuapp.com/database.json?service="+ param;
+        console.log(param);
+        var request = new XMLHttpRequest();
+        request.open("GET", url, true);
+         request.onreadystatechange = function() {
+           if (request.readyState == 4 && request.status == 200) {
+             response = request.responseText;
+             parsed = JSON.parse(response);
+             console.log("AAAAAAAAAfter parsed");
+             for (var i = 0; i < parsed.length; i++) {
+               console.log("i:", i)
+               var latLng = new google.maps.LatLng(parsed[i][1], parsed[i][2])
+               var marker = new google.maps.Marker({
+                 position: latLng,
+                 map: map,
+                 icon: icons[param].url
+               });
+             }
+           }
+         }
+         request.send();
+
 }
